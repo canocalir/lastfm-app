19d33777000064b6cee593bdeb909961e0f699c4
Can Berk Ocalir <can.ocalir@gmail.com>
2022-11-24 22:38:46 +0300

    removed unnecessary imports

--- src/components/ArtistCard/ArtistCard.jsx
+++ src/components/ArtistCard/ArtistCard.jsx
@@ -1,8 +1,6 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import {
-  selectDetails,
   setAlbumsData,
   setTracksData,
 } from "../../features/detailSlice";
